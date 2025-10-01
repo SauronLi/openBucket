456821b84dda542572c7b2b7b79c348dc8aa02d8
SauronLi <imhom@outlook.com>
2025-10-01 13:15:10 +0800

    added MusicBox manifest

--- fixGithub.ps1
+++ fixGithub.ps1
@@ -1,7 +1,6 @@
-# 仅替换未加速的 github.com 链接
+# 仅替换 json 文件中 url 字段的未加速 github.com 链接
 Get-ChildItem -Path .\bucket\ -Filter *.json | ForEach-Object {
     $content = Get-Content $_.FullName -Raw
-    # 只替换未包含 hk.gh-proxy.com 的 github.com 链接
-    $newContent = $content -replace '(?<!hk\.gh-proxy\.com\/https:\/\/github\.com)', 'https://hk.gh-proxy.com/https://github.com'
+    $newContent = $content -replace '("url"\s*:\s*")https://github.com(?!/hk\.gh-proxy\.com)([^"]*)"', '${1}https://hk.gh-proxy.com/https://github.com${3}"'
     Set-Content $_.FullName -Value $newContent
 }
